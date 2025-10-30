43066a54fa5ce77778efad9cebe7368b25472434
Alexader Trutanov <uliza.fonar.apteka@gmail.com>
2025-10-30 22:13:30 +0100

    Adds temporary image on BooksView

--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <h1 class="text-3xl">It's alive, alive</h1>
-    <div class="text-sm">Welcome to the authorized zone</div>
+  <div class="flex flex-col justify-center items-center">
+    <h1 class="text-3xl mb-6">It's alive, alive!</h1>
+    <img class="mb-6 rounded-2xl" src="@/assets/work.gif" />
+    <div class="text-sm">Welcome to the authorized area</div>
     <div class="text-sm">Do whatever you want, except for the hunger strike</div>
   </div>
 </template>
