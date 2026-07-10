eb8633fe20fd7fc0cb435ea9638e04cbc38376bf
Alexader Trutanov <uliza.fonar.apteka@gmail.com>
2026-07-10 18:34:00 +0200

    Adds active link detection

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,16 +5,35 @@
         <Button size="sm" class="gap-2 min-w-25 hover:opacity-90 transition-opacity cursor-pointer">
           <Plus class="w-4 h-4" />
           <span class="hidden sm:inline">Add Book</span>
-          <!-- TODO Active link detection -->
         </Button>
       </div>
 
       <nav className="flex items-center gap-6">
-        <button class="text-sm font-medium hover:text-primary transition-colors cursor-pointer">Books</button>
+        <button
+          class="text-sm font-medium cursor-pointer"
+          :class="{
+            'text-muted-foreground': $route.name == 'books',
+            'hover:text-gray-500': $route.name != 'books',
+            'transition-colors': $route.name != 'books',
+            'pointer-none': $route.name == 'books',
+          }"
+          @click="$router.push('books')"
+        >
+          Books
+        </button>
         <div class="bg-primary p-2 rounded-lg text-primary-foreground">
           <BookOpen class="w-5 h-5" />
         </div>
-        <button class="text-sm text-muted-foreground font-medium hover:text-primary transition-colors cursor-pointer">
+        <button
+          class="text-sm font-medium cursor-pointer"
+          :class="{
+            'text-muted-foreground': $route.name == 'stats',
+            'hover:text-gray-500': $route.name != 'stats',
+            'transition-colors': $route.name != 'stats',
+            'pointer-none': $route.name == 'stats',
+          }"
+          @click="$router.push('stats')"
+        >
           Stats
         </button>
       </nav>
