ba6e45be183520840a0cdc2703a3ff0b9a4ad176
Alexader Trutanov <uliza.fonar.apteka@gmail.com>
2026-08-10 13:22:12 +0200

    Fixes bug with a scroll color on Y axis when user draws a page

--- src/views/RootView.vue
+++ src/views/RootView.vue
@@ -10,4 +10,18 @@
 import TheHero from '@/components/main/TheHero.vue'
 import TheProcess from '@/components/main/TheProcess.vue'
 import CallToAction from '@/components/main/CallToAction.vue'
+import { useCssVar } from '@vueuse/core'
+import { onMounted, onUnmounted } from 'vue'
+
+/***
+ * Sets main page background color for the part of the page that appears on top and bottom
+ * only when user tries to scroll in this direction outside to the existing part and 'draws' the page
+ * It can be achieved only with body tag
+ */
+function setScrollYColor(isMainPage: boolean) {
+  document.body.style.backgroundColor = isMainPage ? useCssVar('--color-main-bg').value || 'unset' : 'unset'
+}
+
+onMounted(() => setScrollYColor(true))
+onUnmounted(() => setScrollYColor(false))
 </script>
