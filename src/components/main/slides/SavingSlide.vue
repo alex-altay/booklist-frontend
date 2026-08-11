ed022c16da600f750134a8825769c6b0610ca128
Alexader Trutanov <uliza.fonar.apteka@gmail.com>
2026-08-11 17:13:51 +0200

    Moves svg form to 1px to prevent browser artifacts

--- src/components/main/slides/SavingSlide.vue
+++ src/components/main/slides/SavingSlide.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="saving" class="relative shrink-0 flex flex-row h-screen overflow-hidden no-scrollbar bg-main-additional">
-    <img class="h-screen" src="@/assets/main-page/forms/slide-two.svg" />
+    <img class="relative -left-px h-screen" src="@/assets/main-page/forms/slide-two.svg" />
     <img
       class="relative top-[5vh] h-[85vh] left-[-10vw] sm:left-0 animate-float"
       src="@/assets/main-page/illustrations/saving.png"
