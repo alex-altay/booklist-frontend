76e66cbf54e2e819515c7083766ccff5008e4f5d
Alexader Trutanov <uliza.fonar.apteka@gmail.com>
2026-08-18 15:22:33 +0200

    Removes redundant check in useSlideParallax

--- src/composables/useSlideParallax.ts
+++ src/composables/useSlideParallax.ts
@@ -4,10 +4,6 @@ import { computed, type ShallowRef, type ComputedRef } from 'vue'
 export function useSlideParallax(
   target: Readonly<ShallowRef<HTMLDivElement | null>>,
 ): ComputedRef<{ roll: number; tilt: number }> {
-  if (!target) {
-    return computed(() => ({ roll: 0, tilt: 0 }))
-  }
-
   const { isOutside } = useMouseInElement(target)
   const parallax = useParallax(target)
 
