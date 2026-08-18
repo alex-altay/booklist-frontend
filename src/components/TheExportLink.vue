d58af9ccfbca5c3c89c19cc2e96b3c3cd07e007a
Alexader Trutanov <uliza.fonar.apteka@gmail.com>
2026-08-18 14:29:53 +0200

    Fixes bug with an empty store on books export

--- src/components/TheExportLink.vue
+++ src/components/TheExportLink.vue
@@ -1,12 +1,7 @@
 <template>
-  <a
-    :href
-    :download="fileName"
-    class="hover:text-primary transition-colors cursor-pointer select-none"
-    @click="downloadBooks"
-  >
+  <button class="hover:text-primary transition-colors cursor-pointer select-none" @click="downloadBooks">
     Data Export
-  </a>
+  </button>
 </template>
 
 <script setup lang="ts">
@@ -14,18 +9,24 @@ import { useBookStore } from '@/stores/book'
 import { booksToCSV } from '@/utils/booksToCSV'
 import { toast } from 'vue-sonner'
 import { useApi } from '@/composables/useApi'
-import { ref } from 'vue'
 
 const fileName = 'export.csv'
-const href = ref(fileName)
+const { request, error } = useApi()
 
 async function downloadBooks() {
   const promise = async () => {
-    const { request } = useApi()
     const books = (await request(() => useBookStore().getBooks())) || []
+    if (error.value) {
+      throw new Error(error.value)
+    }
     const content = booksToCSV(books)
     const file = new Blob([content], { type: 'text/csv;charset=utf-8' })
-    href.value = URL.createObjectURL(file)
+    const url = URL.createObjectURL(file)
+    const link = document.createElement('a')
+    link.href = url
+    link.download = fileName
+    link.click()
+    setTimeout(() => URL.revokeObjectURL(url), 0)
   }
   toast.promise(promise, {
     loading: 'Preparing your books...',
