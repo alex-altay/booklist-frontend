58f6b64121b61dd57b6c9e9b9daa935d62ad8b52
Alexader Trutanov <uliza.fonar.apteka@gmail.com>
2026-08-24 16:39:30 +0200

    Sets zod in jitless mode for security headers

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import '@/styles/style.css'
+import * as z from 'zod'
 import App from '@/App.vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { router } from '@/router/router'
 
+z.config({ jitless: true })
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
