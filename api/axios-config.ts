a65e7610f869c3be1d6036c8059d0310891d035e
Alexader Trutanov <uliza.fonar.apteka@gmail.com>
2026-08-24 16:01:12 +0200

    Adds more security headers. Stage 1

--- api/axios-config.ts
+++ api/axios-config.ts
@@ -1,12 +1,11 @@
 import { ERRORS } from '@/errors/pure'
-import axios, { type AxiosRequestConfig, type AxiosError, type AxiosResponse } from 'axios'
+import axios, { type AxiosError, type AxiosResponse } from 'axios'
 
 const baseURL = import.meta.env.VITE_HOST
 if (!baseURL) {
   throw new Error(ERRORS.NO_HOST)
 }
 
-const axiosConfig: AxiosRequestConfig = {}
 const _axios = axios.create({ baseURL })
 
 _axios.interceptors.response.use(
@@ -16,29 +15,27 @@ _axios.interceptors.response.use(
 
 const axiosInstance = {
   async get<GetRequest = unknown>(url: string) {
-    return (await _axios.get<GetRequest>(url, axiosConfig)).data
+    return (await _axios.get<GetRequest>(url)).data
   },
 
   async post<PostRequest = unknown, PostResponse = unknown>(url: string, data?: PostRequest): Promise<PostResponse> {
-    return (await _axios.post<PostResponse, AxiosResponse<PostResponse>>(url, data, axiosConfig)).data
+    return (await _axios.post<PostResponse, AxiosResponse<PostResponse>>(url, data)).data
   },
 
   async delete<DeleteResponse = unknown>(url: string) {
-    return (await _axios.delete<DeleteResponse>(url, axiosConfig)).data
+    return (await _axios.delete<DeleteResponse>(url)).data
   },
 
   async patch<PatchRequest = unknown, PatchResponse = unknown>(url: string, data?: PatchRequest) {
-    return (await _axios.patch<PatchResponse>(url, data, axiosConfig)).data
+    return (await _axios.patch<PatchResponse>(url, data)).data
   },
 
   setAuthToken(token: string) {
-    axiosConfig.headers = axiosConfig.headers || {}
-    axiosConfig.headers.Authorization = `Bearer ${token}`
+    _axios.defaults.headers.common.Authorization = `Bearer ${token}`
   },
 
   removeAuthToken() {
-    axiosConfig.headers = axiosConfig.headers || {}
-    axiosConfig.headers.Authorization = ''
+    delete _axios.defaults.headers.common.Authorization
   },
 }
 
